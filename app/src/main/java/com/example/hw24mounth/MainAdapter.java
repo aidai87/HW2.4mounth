e9dd06684d6ab7c1571e394f8bcd35f8a44daf51
aidai87 <aidaiataeva87@gmail.com>
2022-04-02 15:13:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw24mounth/MainAdapter.java
@@ -0,0 +1,52 @@
+package com.example.hw24mounth;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter <MainAdapter.MainViewHolder>{
+private ArrayList<String> data;
+
+    public MainAdapter(ArrayList<String> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_name, parent,false ));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+         holder.bind(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class MainViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView TextView;
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+            TextView = itemView.findViewById(R.id.textView);
+        }
+
+
+        public void bind(String s) {
+            TextView.setText(s);
+        }
+    }
+}
